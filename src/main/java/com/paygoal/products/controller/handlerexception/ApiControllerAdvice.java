e600ff8678c853a5c0eff0f3f9f09328f5e1898f
Santiago Nuñez Ingas <santiago.nunez.ingas@gmail.com>
2023-04-21 11:29:35 -0300

    HandlerException & ResponseDto

---
+++ src/main/java/com/paygoal/products/controller/handlerexception/ApiControllerAdvice.java
@@ -0,0 +1,36 @@
+package com.paygoal.products.controller.handlerexception;
+
+import com.paygoal.products.business.dto.response.ExceptionDto;
+import com.paygoal.products.exception.IdNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@ControllerAdvice(annotations = RestController.class)
+public class ApiControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionDto exceptionHandler(MethodArgumentNotValidException ex) {
+        List<FieldError> errorList = ex.getBindingResult().getFieldErrors();
+        Map<String, String> detalle = new HashMap<>();
+        errorList.forEach(x -> detalle.put(x.getField(), x.getDefaultMessage()));
+        return new ExceptionDto(HttpStatus.BAD_REQUEST.value(), "Error de validación", detalle);
+    }
+
+    @ExceptionHandler(IdNotFoundException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ExceptionDto idNotFound(IdNotFoundException ex) {
+        return new ExceptionDto(HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(),
+                null);
+    }
+
+}
