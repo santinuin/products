8d4ce82037f999eb93acaf32e92b44a8a5a295b1
Santiago Nuñez Ingas <santiago.nunez.ingas@gmail.com>
2023-04-21 19:11:54 -0300

    Logback & Swagger

---
+++ src/main/java/com/paygoal/products/configuration/SpringFoxConfig.java
@@ -0,0 +1,37 @@
+package com.paygoal.products.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SpringFoxConfig {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.paygoal.products.controller"))
+                .paths(PathSelectors.any()).build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfo(
+                "Products API",
+                "APIRest construida con Spring - Backend",
+                "V1",
+                null,
+                new Contact("Santiago", "https://github.com/santinuin", "santiago.nunez.ingas@gmail.com"),
+                null,
+                null,
+                Collections.emptyList()
+        );
+    }
+}
--- src/main/java/com/paygoal/products/controller/ProductController.java
+++ src/main/java/com/paygoal/products/controller/ProductController.java
@@ -5,6 +5,8 @@ import com.paygoal.products.business.mapper.ProductMapper;
 import com.paygoal.products.domain.Product;
 import com.paygoal.products.exception.IdNotFoundException;
 import com.paygoal.products.service.ProductService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +19,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/productos")
+@Api(value = "Product Controller", tags = "Acciones permitidas para productos")
 public class ProductController {
 
     private final ProductService service;
@@ -29,6 +32,9 @@ public class ProductController {
     }
 
     @GetMapping
+    @ApiOperation(value = "Listar todos los productos",
+            notes = "Lista todos los productos, si queremos ordenarlos por precio debemos agregar un parametro booleano," +
+                    " en donde si es true, lo hace de forma ascendente, y si es false lo hace de forma descendente")
     public ResponseEntity<?> list(@RequestParam(required = false) Boolean orderedByPrice) {
 
         if (orderedByPrice == null) {
@@ -59,9 +65,12 @@ public class ProductController {
     }
 
     @GetMapping("/buscar")
+    @ApiOperation(value = "Buscar productos",
+            notes = "Podemos buscar por ID o por Nombre, siempre que se lo especifiquemos en los parametros," +
+                    "(siempre va a poderar ID por sobre el nombre si se especifican ambos)")
     public ResponseEntity<?> findByIdOrName(@RequestParam(required = false) Long id,
-                                            @RequestParam(required = false) String name){
-        if (id != null){
+                                            @RequestParam(required = false) String name) {
+        if (id != null) {
             ProductDto productDto = this.mapper.toDto(this.service.findById(id));
 
             return new ResponseEntity<>(productDto, HttpStatus.OK);
@@ -73,6 +82,7 @@ public class ProductController {
     }
 
     @PostMapping("/crear")
+    @ApiOperation(value = "Crear producto nuevo")
     public ResponseEntity<?> create(@Valid @RequestBody ProductDto productDto) {
 
         Map<String, Object> response = new HashMap<>();
@@ -86,6 +96,7 @@ public class ProductController {
     }
 
     @PutMapping("/{id}")
+    @ApiOperation(value = "Modificar producto")
     public ResponseEntity<?> update(@PathVariable Long id,
                                     @Valid @RequestBody ProductDto productDto) throws IdNotFoundException {
 
@@ -100,6 +111,7 @@ public class ProductController {
     }
 
     @DeleteMapping("eliminar/{id}")
+    @ApiOperation(value = "Eliminar producto")
     public ResponseEntity<?> deleteById(@PathVariable Long id) throws IdNotFoundException {
 
         Map<String, Object> response = new HashMap<>();
