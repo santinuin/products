85e1f3a5d331043a6c17f2c5fbcf94f71450f2a5
Santiago Nuñez Ingas <santiago.nunez.ingas@gmail.com>
2023-04-21 00:18:25 -0300

    Entity and Load-SQL

---
+++ src/main/java/com/paygoal/products/domain/entity/Product.java
@@ -0,0 +1,80 @@
+package com.paygoal.products.domain.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 30)
+    @Size(max = 30)
+    @NotBlank
+    private String name;
+
+    @Size(max = 255)
+    private String description;
+
+    @Column(precision = 10, scale = 2)
+    @NotBlank
+    private BigDecimal price;
+
+    private Integer quantity;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String description, BigDecimal price, Integer quantity) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+/* Populate table products */
+
+INSERT INTO products (name, description, price, quantity) VALUES ('Bicicleta', 'Las mejores bicis del mercado', 70000.00, 40);
+INSERT INTO products (name, description, price, quantity) VALUES ('Smart TV', 'Tv con calidad Full HD', 90000.99, 20);
+INSERT INTO products (name, description, price, quantity) VALUES ('Sillón', 'Cubierto en cuero, máximo confort', 65000.00, 17);
+INSERT INTO products (name, description, price, quantity) VALUES ('Escritorio', 'Escritorio moderno y funcional', 18000, 12);
+INSERT INTO products (name, description, price, quantity) VALUES ('Sommier', 'Comodo, durable y firme', 72000.80, 5);
